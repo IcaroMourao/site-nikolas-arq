ec6754b5c73765e4f939127514afe036ce6fb587
imourao <imourao@nelogica.com.br>
2020-04-18 14:14:32 -0300

    Adiciona estilos iniciais #4

--- src/App.vue
+++ src/App.vue
@@ -12,13 +12,9 @@ export default {
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import "./global/scss/base-styles.scss";
+@import "../node_modules/bootstrap/scss/bootstrap.scss";
+@import "../node_modules/bootstrap-vue/src/index.scss";
+
 </style>

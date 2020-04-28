0503b285813f9bba3327044466761ff83a727508
imourao <icaro.mourao1@hotmail.com>
2020-04-27 23:54:01 -0300

    Adiciona novos ícones para o footer #issue12

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,12 @@
 <template>
   <footer id="footer">
     <b-row>
-      <b-col cols="12" sm="6">
+      <b-col cols="12" sm="4">
+        <router-link to="/" title="Nikolas Mourão">
+          <img src="../assets/images/name.png" alt="Nikolas Mourão">
+        </router-link>
+      </b-col>
+      <b-col cols="12" sm="4">
         <div class="footer-info">
           <a class="footer-personal">Nikolas de Sena Mourão</a>
           <a class="footer-personal">Brasilia de todos os santo, Brasil</a>
@@ -9,21 +14,24 @@
           <a class="footer-personal">nikolas@yahoo.com.br</a>
         </div>
       </b-col>
-      <b-col cols="12" sm="6">
+      <b-col cols="12" sm="4">
         <b-row no-gutters>
           <div class="social-group">
-            <div class="facebook-img"/>
-            <div class="instagram-img"/>
-            <div class="linkedin-img"/>
+            <a class="facebook-img" href="https://www.facebook.com/" target="_blank"/>
+            <a class="instagram-img circle" href="https://www.instagram.com/" target="_blank"/>
+            <a class="linkedin-img circle" href="https://www.linkedin.com/" target="_blank"/>
           </div>
         </b-row>
       </b-col>
+    </b-row>
+    <b-row>
+      <a class="footer-copyright">Copyright © 2020 Nikolas Mourão. Todos direitos reservados.</a>
     </b-row>
   </footer>
 </template>
 
 
-<style lang="scss">
+<style lang="scss" scoped>
 #footer {
   background: $background-header-footer;
   width: 100%;
@@ -33,8 +41,6 @@
     margin: 0;
   }
   .footer-info {
-    display: grid;
-    justify-content: center;
     padding-bottom: 1.5rem;
     a {
       display: block;
@@ -47,21 +53,62 @@
     .facebook-img {
       width: 48px;
       height: 48px;
-      background: url('../assets/icons/facebook-blue.svg');
+      background: url('../assets/icons/facebook.svg');
+      background-size: 48px 48px;
+      background-repeat: no-repeat;
+    }
+    .facebook-img:hover {
+      width: 48px;
+      height: 48px;
+      background: url('../assets/icons/facebook-hover.svg');
+      background-size: 48px 48px;
+      background-repeat: no-repeat;
     }
     .instagram-img {
+      margin-left: 1rem;
       width: 48px;
       height: 48px;
-      margin-left: 1.5rem;
-      background: url('../assets/icons/instagram-colorful.svg');
+      background: url('../assets/icons/instagram.svg');
+      background-size: 48px 48px;
+      background-repeat: no-repeat;
+    }
+    .instagram-img:hover {
+      margin-left: 1rem;
+      width: 48px;
+      height: 48px;
+      background: url('../assets/icons/instagram-hover.svg');
+      background-size: 48px 48px;
+      background-repeat: no-repeat;
     }
     .linkedin-img {
+      margin-left: 1rem;
       width: 48px;
       height: 48px;
-      margin-left: 1.5rem;
-      background: url('../assets/icons/linkedin-blue.svg');
+      background: url('../assets/icons/linkedin.svg');
+      background-size: 48px 48px;
+      background-repeat: no-repeat;
+    }
+    .linkedin-img:hover {
+      margin-left: 1rem;
+      width: 48px;
+      height: 48px;
+      background: url('../assets/icons/linkedin-hover.svg');
+      background-size: 48px 48px;
+      background-repeat: no-repeat;
     }
   }
+  .footer-copyright {
+    position: absolute;
+    right: 0;
+    color: $normal-text;
+    padding-right: 2rem;
+    font-size: .875rem;
+  }
+}
+
+img {
+  max-height: 100px;
+  outline: none;
 }
 
 @media(min-width: map-get($grid-breakpoints, "sm")) {

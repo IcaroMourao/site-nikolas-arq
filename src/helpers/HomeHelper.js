9b979367b2a2379aeac25bd0573881e62d7c3cda
imourao <icaro.mourao1@hotmail.com>
2020-05-13 20:25:54 -0300

    Ajusta imagens da home #issue22

--- src/helpers/HomeHelper.js
+++ src/helpers/HomeHelper.js
@@ -4,7 +4,7 @@ export default class HomeHelper {
   static items = [
     {
       name: 'image1',
-      img: require('@/assets/images/home/slider/image1.jpeg'),
+      img: require('@/assets/images/home/slider/image1.jpg'),
     },
     {
       name: 'image2',
@@ -24,7 +24,7 @@ export default class HomeHelper {
     },
     {
       name: 'image6',
-      img: require('@/assets/images/home/slider/image5.jpg'),
+      img: require('@/assets/images/home/slider/image6.jpg'),
     },
   ];
 }

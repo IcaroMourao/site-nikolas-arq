a19eaa7816d4c261bf3216d0ab179717544c26ee
imourao <icaro.mourao1@hotmail.com>
2021-02-08 08:42:27 -0300

    Cria banner na tela inicial #develop

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,60 +1,69 @@
 <template>
-  <div class="home-container app-container">
-    <div class="home-projects-featured-title">
-      <h1 class="title"> Projetos em destaque </h1>
+  <div class="home-container">
+    <div class="home-banner">
+      <div class="video-wrapper">
+        <video autoplay muted loop>
+          <source :src="require('@/assets/videos/home-banner-logo.mp4')" type="video/mp4" />
+        </video>
+      </div>
     </div>
-    <div class="projects-featured">
-      <div class="project-wrapper project-one">
-        <div class="project-infos one-infos">
-          <h3>Suíte Rotang</h3>
-          <p>
-            A seleção do mobiliário e a presença da madeira clara e rattan em todo o ambiente,
-            com acabamento de filetes metalizados e no piso, além das lâminas de madeira, as
-            paredes também foram revestidas com ripas de madeiras claras.
-          </p>
-          <div @click="goTo(3)" class="arrow-div">
-            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15" style="transform:rotate(180deg);" class="arrow"><path fill-rule="evenodd" d="M7.5 15L0 7.5 7.5 0l1.325 1.325-5.232 5.254H15v1.842H3.593l5.232 5.254z"></path></svg>
-          </div>
-        </div>
-        <div class="project-image image-one">
-          <img
-            :src="require('@/assets/images/projects/project3/01.jpg')"
-            alt="01">
-        </div>
+    <div class="app-container">
+      <div class="home-projects-featured-title">
+        <h1 class="title"> Projetos em destaque </h1>
       </div>
-      <div class="project-wrapper project-two ">
-        <div class="project-infos two-infos">
-          <h3>Santuário Natural</h3>
-          <p>
-            Um verdadeiro santuário de relaxamento, com materiais naturais como pedra e madeira
-            proporcionam um equilíbrio minimalista e aconchegante.
-          </p>
-          <div @click="goTo(4)" class="arrow-div">
-            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15" style="transform:rotate(180deg);" class="arrow"><path fill-rule="evenodd" d="M7.5 15L0 7.5 7.5 0l1.325 1.325-5.232 5.254H15v1.842H3.593l5.232 5.254z"></path></svg>
+      <div class="projects-featured">
+        <div class="project-wrapper project-one">
+          <div class="project-infos one-infos">
+            <h3>Suíte Rotang</h3>
+            <p>
+              A seleção do mobiliário e a presença da madeira clara e rattan em todo o ambiente,
+              com acabamento de filetes metalizados e no piso, além das lâminas de madeira, as
+              paredes também foram revestidas com ripas de madeiras claras.
+            </p>
+            <div @click="goTo(3)" class="arrow-div">
+              <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15" style="transform:rotate(180deg);" class="arrow"><path fill-rule="evenodd" d="M7.5 15L0 7.5 7.5 0l1.325 1.325-5.232 5.254H15v1.842H3.593l5.232 5.254z"></path></svg>
+            </div>
+          </div>
+          <div class="project-image image-one">
+            <img
+              :src="require('@/assets/images/projects/project3/01.jpg')"
+              alt="01">
           </div>
         </div>
-        <div class="project-image image-two">
-          <img
-            :src="require('@/assets/images/projects/project4/04.jpg')"
-            alt="01">
-        </div>
-      </div>
-      <div class="project-wrapper project-three">
-        <div class="project-infos three-infos">
-          <h3>Office in house</h3>
-          <p>
-            Ambientes funcionais e integrados são a nova tendência em casas e principalmente
-            apartamentos. Assim como os home offices que estão cada vez mais comuns devido a
-            tecnologia e a diversos trabalhos que dependem dela.
-          </p>
-          <div @click="goTo(2)" class="arrow-div">
-            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15" style="transform:rotate(180deg);" class="arrow"><path fill-rule="evenodd" d="M7.5 15L0 7.5 7.5 0l1.325 1.325-5.232 5.254H15v1.842H3.593l5.232 5.254z"></path></svg>
+        <div class="project-wrapper project-two ">
+          <div class="project-infos two-infos">
+            <h3>Santuário Natural</h3>
+            <p>
+              Um verdadeiro santuário de relaxamento, com materiais naturais como pedra e madeira
+              proporcionam um equilíbrio minimalista e aconchegante.
+            </p>
+            <div @click="goTo(4)" class="arrow-div">
+              <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15" style="transform:rotate(180deg);" class="arrow"><path fill-rule="evenodd" d="M7.5 15L0 7.5 7.5 0l1.325 1.325-5.232 5.254H15v1.842H3.593l5.232 5.254z"></path></svg>
+            </div>
+          </div>
+          <div class="project-image image-two">
+            <img
+              :src="require('@/assets/images/projects/project4/04.jpg')"
+              alt="01">
           </div>
         </div>
-        <div class="project-image image-three">
-          <img
-            :src="require('@/assets/images/projects/project2/06.jpg')"
-            alt="01">
+        <div class="project-wrapper project-three">
+          <div class="project-infos three-infos">
+            <h3>Office in house</h3>
+            <p>
+              Ambientes funcionais e integrados são a nova tendência em casas e principalmente
+              apartamentos. Assim como os home offices que estão cada vez mais comuns devido a
+              tecnologia e a diversos trabalhos que dependem dela.
+            </p>
+            <div @click="goTo(2)" class="arrow-div">
+              <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15" style="transform:rotate(180deg);" class="arrow"><path fill-rule="evenodd" d="M7.5 15L0 7.5 7.5 0l1.325 1.325-5.232 5.254H15v1.842H3.593l5.232 5.254z"></path></svg>
+            </div>
+          </div>
+          <div class="project-image image-three">
+            <img
+              :src="require('@/assets/images/projects/project2/06.jpg')"
+              alt="01">
+          </div>
         </div>
       </div>
     </div>
@@ -95,6 +104,28 @@ export default {
 }
 
 .home-container {
+  .home-banner {
+    width: 100%;
+    height: 500px;
+    background: url('~@/assets/images/home/home-banner.png');
+    background-size: 100%;
+    background-attachment: fixed;
+    background-repeat: no-repeat;
+    .video-wrapper {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      width: 100%;
+      height: 100%;
+      background: black;
+      opacity: 0.5;
+      vertical-align: middle;
+      video {
+        width: 100%;
+        height: 350px;
+      }
+    }
+  }
   .home-projects-featured-title {
     margin-bottom: 4rem;
     h1 {

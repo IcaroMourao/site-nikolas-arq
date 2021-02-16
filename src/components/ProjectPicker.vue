d80c27d9901fa04c0c69a48041a692156ef2f20c
imourao <imourao@nelogica.com.br>
2021-02-16 09:52:52 -0300

    Adiciona ícone de curtir e compartilhar no seletor de projetos #issue37

--- src/components/ProjectPicker.vue
+++ src/components/ProjectPicker.vue
@@ -11,18 +11,13 @@
           :key="index">
           <img
             @click="handlerLicense(project)"
-            @mouseover="project.showName = true"
-            @mouseout="project.showName = false"
-            :class="{'img-hover': project.showName}"
-            :src="getProjectImage(project)"
+            class="project-image" :src="getProjectImage(project)"
             :alt="project.name">
-          <p
-            @click="handlerLicense(project)"
-            @mouseover="project.showName = true"
-            @mouseout="project.showName = false"
-            v-show="project.showName" class="projects-item-text">
-              {{ project.name }}
+          <p @click="handlerLicense(project)" class="projects-item-text">
+            {{ project.name }}
           </p>
+          <div class="heart-icon" />
+          <div class="share-icon" />
         </div>
       </div>
     </div>
@@ -69,31 +64,78 @@ export default {
       color: $current;
       background-color: $app-background;
       font-weight: bold;
-      img {
-        max-width: 280px;
-        margin: 1rem;
-        border-radius: 10px;
-        box-shadow: 0px 6px 6px 0px rgba(0,0,0,.16);
-        &:hover {
-          cursor: pointer;
+      margin: 1rem;
+      &:hover {
+        cursor: pointer;
+        & > .project-image {
           opacity: 0.3;
         }
+        & > .projects-item-text {
+          display: block;
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          font-size: 2rem;
+          cursor: pointer;
+          user-select: none;
+        }
+        & > .heart-icon {
+          display: block;
+          position: absolute;
+          bottom: 35px;
+          left: 35px;
+          width: 24px;
+          height: 24px;
+          background: url('~@/assets/icons/heart.svg');
+          background-size: 24px 24px;
+          background-repeat: no-repeat;
+          &:hover {
+            width: 24px;
+            height: 24px;
+            background: url('~@/assets/icons/heart-hover.svg');
+            background-size: 24px 24px;
+            background-repeat: no-repeat;
+          }
+        }
+        & > .share-icon {
+          display: block;
+          position: absolute;
+          bottom: 35px;
+          right: 35px;
+          width: 24px;
+          height: 24px;
+          background: url('~@/assets/icons/share.svg');
+          background-size: 24px 24px;
+          background-repeat: no-repeat;
+          &:hover {
+            width: 24px;
+            height: 24px;
+            background: url('~@/assets/icons/share-hover.svg');
+            background-size: 24px 24px;
+            background-repeat: no-repeat;
+          }
+        }
+      }
+      .project-image {
+        max-width: 100%;
+        border-radius: 10px;
+        box-shadow: 0px 6px 6px 0px rgba(0,0,0,.16);
         @media (min-width: map-get($grid-breakpoints, "lg")){
           max-width: 420px;
         }
       }
-      .img-hover {
-        opacity: 0.3;
-      }
       .projects-item-text {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        font-size: 2rem;
-        &:hover {
-          cursor: pointer;
-        }
+        display: none;
+      }
+      .heart-icon {
+        display: none;
+      }
+      .heart-icon {
+        display: none;
+      }
+      .share-icon {
+        display: none;
       }
     }
   }

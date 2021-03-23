b3a42de5b9335c8dcdaf836a90c3f18ce83d9389
imourao <imourao@nelogica.com.br>
2021-03-22 22:49:34 -0300

    Implementa seção de serviços na página inicial #issue49

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -67,13 +67,17 @@
         </div>
       </div>
     </div>
+    <Contacts />
   </div>
 </template>
 
 <script>
+import Contacts from '@/components/Contacts.vue';
+
 export default {
   name: 'Home',
   components: {
+    Contacts,
   },
   methods: {
     goTo(projectId) {
